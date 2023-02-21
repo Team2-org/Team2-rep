7cdc945445053d4698b8c6ebe53243fe24c956e9
Gitanjali Sahoo <gr.sahoo15@gmail.com>
2023-02-21 12:54:59 +0100

    navbar created

--- src/main.js
+++ src/main.js
@@ -2,10 +2,16 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
-import "./assets/main.css";
+import "./assets/base.css";
+
+// Import bootstrap for css and js
+import "bootstrap/dist/css/bootstrap.css";
+import bootstrap from "bootstrap/dist/js/bootstrap.bundle.js";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 const app = createApp(App);
 
 app.use(router);
+app.use(bootstrap);
 
 app.mount("#app");
