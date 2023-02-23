2e022f162d0bdf2ea3ff0e64da563e7c325c9d22
”ThereseHa” <haapanentherese@gmail.com>
2023-02-23 11:12:22 +0100

    Json files corretion

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import NavBar from "./components/NavBar.vue";
+import FilterSection from "./components/FilterSection.vue";
 </script>
 
 <template>
   <NavBar />
+  <FilterSection />
 </template>
 
 <style scoped></style>
---
+++ src/components/FilterSection.vue
@@ -0,0 +1,23 @@
+<!--Only made the filter button for now, put in a placeholder place-->
+
+<script></script>
+<template>
+  <input class="filterButton" type="button" value="Filter" />
+</template>
+
+<style scoped>
+.filterButton {
+  display: flex;
+  margin-top: 100px;
+  justify-content: center;
+
+  padding: 9px 15px;
+
+  width: 133px;
+  height: 48px;
+
+  border-width: 1px 0px;
+  border-style: solid;
+  border-color: #000000;
+}
+</style>
