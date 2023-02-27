7523a9eb87e81b7f2be4b09305ab3a04de40748c
”ThereseHa” <haapanentherese@gmail.com>
2023-02-27 11:09:43 +0100

    Corrected FilterSection placement

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,10 @@
 <script setup>
 import NavBar from "./components/NavBar.vue";
-import FilterSection from "./components/FilterSection.vue";
 import CompFooter from "./components/CompFooter.vue"
 </script>
 
 <template>
   <NavBar />
-  <FilterSection />
   <CompFooter />
 </template>
 
