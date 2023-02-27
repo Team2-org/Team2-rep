fb22f783ce11dbc3bf6e504491d1440c70ae68b5
”ThereseHa” <haapanentherese@gmail.com>
2023-02-27 11:18:35 +0100

    fixed app.vue?

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,11 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
 import CompFooter from "./components/CompFooter.vue";
-
 import NavBar from "./components/NavBar.vue";
-import CompFooter from "./components/CompFooter.vue"
 </script>
-
 
 <template>
   <NavBar />
-    <RouterView />
   <CompFooter />
 </template>
 
