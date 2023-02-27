0a50184343c138b0e43ce51843b58ded1d738abb
Jennygl <112472232+Jennygl@users.noreply.github.com>
2023-02-27 11:02:55 +0100

    Update App.vue
    App.vue updated

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,16 @@
 <script setup>
-<<<<<<< HEAD
 import { RouterLink, RouterView } from "vue-router";
 import CompFooter from "./components/CompFooter.vue";
-</script>
-
-<template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
-  <CompFooter />
-=======
 import NavBar from "./components/NavBar.vue";
 import FilterSection from "./components/FilterSection.vue";
 </script>
 
+
 <template>
   <NavBar />
   <FilterSection />
->>>>>>> 1eb4384662b38ba5a6e42a418292fb17eadf1be1
+    <RouterView />
+  <CompFooter />
 </template>
 
 <style scoped></style>
