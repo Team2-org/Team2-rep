6c890c21ff3b4e90594964eb41aa8bc55d7e5c05
Osman Adem <112473033+OsmanAdem@users.noreply.github.com>
2023-03-02 10:58:25 +0100

    Changes

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,4 @@
 <script setup>
-<<<<<<< HEAD
-import { RouterLink, RouterView } from "vue-router";
-import CompFooter from "./components/CompFooter.vue";
-</script>
-
-<template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
-  <CompFooter />
-=======
 import NavBar from "./components/NavBar.vue";
 import FilterSection from "./components/FilterSection.vue";
 </script>
@@ -34,7 +6,6 @@ import FilterSection from "./components/FilterSection.vue";
 <template>
   <NavBar />
   <FilterSection />
->>>>>>> 1eb4384662b38ba5a6e42a418292fb17eadf1be1
 </template>
 
 <style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-<script setup></script>
-
-<template>
-  <main></main>
-=======
 <script setup>
 import TheCard from "../components/TheCard.vue";
 import HeroImage from "../components/HeroImage.vue";
@@ -14,7 +8,6 @@ import CartAccordion from "../components/CartAccordion.vue";
   <HeroImage />
   <TheCard />
   <CartAccordion />
->>>>>>> 1eb4384662b38ba5a6e42a418292fb17eadf1be1
 </template>
 <script></script>
 
