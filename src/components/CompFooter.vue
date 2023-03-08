d7049f8d1d0c90fbca7504d3175379f5bd6773b3
Gitanjali Sahoo <gr.sahoo15@gmail.com>
2023-03-08 15:43:59 +0100

    New footer

--- src/components/CompFooter.vue
+++ src/components/CompFooter.vue
@@ -1,37 +1,56 @@
 <template>
-  <footer class="compFooter">
-    <div class="leftContainer">
-      <div>
-        <div class="iconContainer">
-          <a href="https://www.instagram.com/" target="_blank">
-            <img src="../assets/img/instagram.png" alt="Instagram" />
-          </a>
-          <a href="https://www.facebook.com/" target="_blank">
-            <img src="../assets/img/facebook.png" alt="Facebook" />
-          </a>
-        </div>
-      </div>
-      <div class="infoContainer">
-        <div class="footerInfo">
-          <h3>{{ Text1 }}</h3>
-          <p>About Us</p>
-          <p>Contact Us</p>
-          <p>Social Responsibility</p>
-        </div>
-        <div class="footerInfo">
-          <h3>{{ Text2 }}</h3>
-          <p>Privacy Policy</p>
-          <p>Cookies</p>
+  <div id="footer" class="container">
+    <div class="row">
+      <div class="col-sm">
+        <ul class="footer-list">
+          <h4>{{ Text1 }}</h4>
+          <router-link class="contact-link" :to="{ path: `/about` }"
+            ><li>About Us</li>
+          </router-link>
 
-        </div>
-        <div class="footerInfo">
-          <h3>{{ Text3 }}</h3>
-          <p>Guarantees</p>
-          <p>Terms of purchase</p>
-        </div>
+          <router-link class="contact-link" :to="{ path: `/contact` }"
+            ><li>Contact Us</li>
+          </router-link>
+          <li>Social Responsibility</li>
+          <li>Partner With Us</li>
+
+          <li>Blog</li>
+
+          <router-link class="contact-link" :to="{ path: `/` }"
+            ><li>Home</li>
+          </router-link>
+        </ul>
+      </div>
+      <div class="col-sm">
+        <ul class="footer-list">
+          <h4>{{ Text2 }}</h4>
+          <li>Privacy Policy</li>
+          <li>Cookies</li>
+          <li>Cookie settings</li>
+        </ul>
+      </div>
+
+      <div class="col">
+        <ul class="footer-list">
+          <h4>{{ Text3 }}</h4>
+          <li>Guarantees</li>
+          <li>Terms of purchase</li>
+        </ul>
       </div>
     </div>
-  </footer>
+  </div>
+
+  <div class="icon">
+    <a href="https://www.instagram.com/" target="_blank"
+      ><i class="bi bi-instagram"></i
+    ></a>
+    <a href="https://www.facebook.com/" target="_blank">
+      <i class="bi bi-facebook"></i>
+    </a>
+    <a href="https://www.twitter.com/" target="_blank">
+      <i class="bi bi-twitter"></i>
+    </a>
+  </div>
 </template>
 
 <script>
@@ -45,44 +64,44 @@ export default {
   },
 };
 </script>
-
 <style scoped>
-.compFooter {
-  display: flex;
-  height: 15%;
-  background-color: white;
-  padding-top: 20px;
+#footer {
+  margin-top: 20px;
 }
-.leftContainer {
+.footer-list {
+  list-style: none;
+}
+.icon {
+  font-size: 1.9rem;
   display: flex;
-  align-items: center;
-  gap: 50px;
-  padding-left: 50px;
-  width: 60%;
+  flex-direction: row;
+  justify-content: center;
+}
+.bi {
+  color: black;
+  padding-right: 10px;
+}
+.contact-link {
+  text-decoration: none;
+  color: black;
+}
+.d-container {
+  justify-content: flex-start;
+  align-items: flex-start;
 }
 
-.leftContainer img {
-  height: 30px;
-  width: 30px;
+@media only screen and (max-width: 575px) {
+  ol,
+  ul,
+  dl ul {
+    margin-left: 30%;
+  }
 }
-.rightContainer {
-  width: 40%;
-}
-
-.footerInfo {
-  display: flex;
-  flex-direction: column;
-}
-
-.iconContainer {
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-}
-
-.infoContainer {
-  display: flex;
-  gap: 100px;
-  flex-wrap: wrap;
+@media only screen and (max-width: 430px) {
+  ol,
+  ul,
+  dl ul {
+    margin-left: 25%;
+  }
 }
 </style>
---
+++ src/components/ContactUs.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="contact">
+  <div>
+  <img src="../../public/img/glasses-gcc69f2bed_640.jpg" alt=""></div>
+    <div><h3>World!</h3></div>
+
+  </div>
+</template>
+<style scoped>
+.contact {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+}
+
+</style>
