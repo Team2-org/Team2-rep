078932530eac1fda63f71862cedc2e14eca230be
”ThereseHa” <haapanentherese@gmail.com>
2023-03-08 09:57:02 +0100

    Checkout WIP

--- src/views/CheckoutView.vue
+++ src/views/CheckoutView.vue
@@ -43,6 +43,61 @@
         </div>
       </div>
     </div>
+    <div id="delivery-method">2. Delivery method</div>
+    <div class="delivery-login-container">
+      <span id="account">Already have an account?</span>
+      <span id="account-text">Sign in for a faster checkout experience.</span>
+      <input type="button" class="signin-button" value="Sign in" />
+    </div>
+    <div class="delivery-options">
+      <div class="radio-wrapper">
+        <label for="radioA" class="radio"
+          ><input type="radio" name="radio1" value="Free" /> Free<span
+            class="radio-text"
+            >1 - 3 days delivery to your closest pick-up point</span
+          ></label
+        ><br />
+        <label for="radioB" class="radio"
+          ><input type="radio" name="radio1" value="B" /> 79sek
+          <span class="radio-text">Next day home delivery</span></label
+        ><br />
+      </div>
+    </div>
+    <div id="shipping-address">3. Shipping address</div>
+    <div class="shipping-container">
+      <form class="shipping-form">
+        <div class="col-50">
+          <label for="fname"> First name</label>
+          <input type="text" id="fname" name="firstname" placeholder="John" />
+          <label for="lname"> Last name</label>
+          <input type="text" id="lname" name="lastname" placeholder="Doe" />
+          <label for="adr"> Address</label>
+          <input
+            type="text"
+            id="adr"
+            name="address"
+            placeholder="542 W. 15th Street"
+          />
+          <label for="city">City</label>
+          <input type="text" id="city" name="city" placeholder="New York" />
+          <label for="postal">Postal code</label>
+          <input type="text" id="postal" name="postal" placeholder="12345" />
+          <label for="phone">Phone number</label>
+          <input
+            type="text"
+            id="phone"
+            name="phone"
+            placeholder="070 123 45 67"
+          />
+          <label for="checkbox" class="save-checkbox">
+            <input type="checkbox" name="checkbox" /> Save this information for
+            next time</label
+          >
+        </div>
+      </form>
+    </div>
+    <div id="payment-method">4. Payment method</div>
+    <div class="payment-container"></div>
   </div>
 </template>
 
@@ -74,14 +129,21 @@ export default {
   width: 100vw;
   height: 100%;
   transition: ease all 0.2s;
+  font-family: Quicksand;
 }
-#your-cart {
+#your-cart,
+#delivery-method,
+#shipping-address,
+#payment-method {
   display: flex;
   justify-content: flex-start;
   transition: ease all 0.2s;
   padding: 1rem 3vw 0;
+  font-weight: 600;
 }
-.your-cart-container {
+.your-cart-container,
+.shipping-container,
+.payment-container {
   display: flex;
   align-items: center;
   justify-content: center;
@@ -91,13 +153,102 @@ export default {
   margin-bottom: 3rem;
   background-color: white;
 }
+.delivery-login-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 90vw;
+  height: 20vh;
+  margin: 0 auto;
+  margin-bottom: 3rem;
+  background-color: white;
+}
+.delivery-options {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  width: 90vw;
+  height: 30vh;
+  margin: 0 auto;
+  margin-top: -2rem;
+  margin-bottom: 3rem;
+  background-color: white;
+}
+.delivery-login-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+#account,
+#account-text {
+  color: black;
+}
+#account {
+  font-weight: 600;
+}
+#account-text {
+  font-size: small;
+}
+.signin-button {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  font-family: "Quicksand", sans-serif;
+  font-size: 1.2rem;
+  margin-top: 0vw;
+  width: 10rem;
+  height: 3rem;
+  margin: 1vw;
+  background: rgba(134, 179, 147, 0.57);
+  border-radius: 5px;
+  border: none;
+}
 
+.radio-wrapper {
+  margin-left: 1rem;
+}
+.radio-text {
+  display: flex;
+  font-size: small;
+}
+.radio {
+  border: 1px solid #ebebeb;
+  border-radius: 5px;
+  padding: 1rem;
+  width: 80vw;
+  margin-bottom: 5px;
+}
+
+.shipping-container {
+  height: auto;
+  padding: 1rem;
+}
+.col-50 {
+  display: flex;
+  flex-direction: column;
+  min-width: 80vw;
+}
+input::placeholder {
+  color: rgb(162, 158, 158);
+}
+input[type="text"] {
+  width: 100%;
+  height: 2.5rem;
+  border: 1px solid rgb(230, 227, 227);
+}
+
+.save-checkbox {
+  font-size: small;
+}
 @media screen and (max-width: 430px) {
   #bar-title {
-    margin-left: 0;
+    margin-left: 1rem;
   }
 }
 
+/*----------Your cart fetch styling here-------------*/
 .card {
   margin: 0;
   width: 50%;
